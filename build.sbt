ef9b07f769bd7b6f3f8dd79a17c280649fe2e42c
Jonathan Hamm <JonathanH@Pennwell.com>
2015-08-04 21:33:29 -0500

    Wrote code for handling posts

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+
 name := "httpserver"
 
 version := "1.0"
@@ -6,3 +7,7 @@ scalaVersion := "2.11.7"
 
 libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.11"
 libraryDependencies += "io.spray" % "spray-client_2.11" % "1.3.3"
+libraryDependencies += "org.scalikejdbc" %% "scalikejdbc" % "2.2.7"
+libraryDependencies += "org.scalikejdbc" %% "scalikejdbc-test" % "2.2.7" % "test"
+libraryDependencies += "com.h2database" % "h2" % "1.4.187"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3"
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -14,7 +14,10 @@ import scala.util.{Try, Success, Failure}
 class Server(port: Int) extends Actor with ActorLogging {
 
   override def receive: Receive = {
-    case _: Http.Connected ⇒ sender ! Http.Register(new ServerRequest().self)
+    case _: Http.Connected ⇒ {
+      val serveActor = Start.system.actorOf(Props(new ServerRequest))
+      sender ! Http.Register(serveActor)
+    }
     case msg ⇒ log.warning("Unkwown message: " + msg)
   }
 
@@ -23,6 +26,7 @@ class Server(port: Int) extends Actor with ActorLogging {
 class ServerRequest extends Actor {
   /* routes */
   val route_home = "/home.html"
+  val route_purchase = "/purchase"
 
   override def receive: Receive = {
 
@@ -32,8 +36,7 @@ class ServerRequest extends Actor {
           val body = buffer.mkString
           sender ! HttpResponse(
             entity = HttpEntity(MediaTypes.`text/html`, body)
-          )
-            .withHeaders(List(
+          ).withHeaders(List(
             HttpHeaders.Connection("close")
           ))
         }
@@ -42,8 +45,23 @@ class ServerRequest extends Actor {
         }
       }
     }
-    case HttpRequest(HttpMethods.GET, unknown, _, _, _) ⇒ {
-      println(s"404: ${unknown} not found")
+    case HttpRequest(HttpMethods.POST, Uri.Path(`route_purchase`), header, entity, _) ⇒ {
+      header.find(_.name == "Content-Type").foreach{ h ⇒
+
+        h.value match {
+          case v if v.contains("application/x-www-form-urlencoded") ⇒ {
+            urlFormEncodeToMap(entity.asString)
+            sender ! HttpResponse()
+          }
+          case mime ⇒ {
+            println("unknown mime type: " + mime)
+          }
+        }
+      }
+    }
+    case HttpRequest(method, unknown, _, _, _) ⇒ {
+      println(s"404: ${method.toString()} : ${unknown.toString()} not found")
+      sender ! HttpResponse(status = StatusCodes.NotFound)
     }
     case x ⇒ {
       println("x: " + x)
@@ -53,6 +71,21 @@ class ServerRequest extends Actor {
   def readPage(path: String) = {
     Try(scala.io.Source.fromFile("web" + path))
   }
+
+  def urlFormEncodeToMap(data: String): Map[String,String] = {
+    println("data: " + data)
+    data.split("&").map { prop ⇒
+      val p = prop.split("=")
+      if(p.length == 2) {
+        p(0) -> p(1)
+      }
+      else {
+        p(0) -> ""
+      }
+    }
+    .toMap[String,String]
+  }
+
 }
 
 object Start {
